adcbac10f6b1f28b3258eba6089043e6b65402f3
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-02 14:47:58 +0800

    Initial Commit

---
+++ SwiftUIStacksPractice/SwiftUIStacksPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksPracticeApp.swift
+//  SwiftUIStacksPractice
+//
+//  Created by 廖昱晴 on 2021/3/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
